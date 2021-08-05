2de9474c505eab59d4fba34e92fd9766bace93fd
Daniel Odievich <daniel.odievich@snowflake.com>
2021-08-04 19:55:29 -0700

    Adding file lists for the release zip archive

--- ZipReleases.ps1
+++ ZipReleases.ps1
@@ -6,12 +6,12 @@ $version = $projXML.SelectNodes("Project/PropertyGroup/Version")."#text"
 $version
 
 cd "bin\Publish\win"
-& $zip a "..\..\..\..\Releases\$version\SnowflakePS.win.$version.zip" '@..\..\..\Release\listfile.win.txt'
+& $zip a "..\..\..\..\Releases\$version\SnowflakePS.win.$version.zip" '@..\..\..\ReleaseIncludes\listfile.win.txt'
 
 cd "..\osx"
-& $zip a "..\..\..\..\Releases\$version\SnowflakePS.osx.$version.zip" '@..\..\..\Release\listfile.osx.txt'
+& $zip a "..\..\..\..\Releases\$version\SnowflakePS.osx.$version.zip" '@..\..\..\ReleaseIncludes\listfile.osx.txt'
 
 cd "..\linux"
-& $zip a "..\..\..\..\Releases\$version\SnowflakePS.linux.$version.zip" '@..\..\..\Release\listfile.linux.txt'
+& $zip a "..\..\..\..\Releases\$version\SnowflakePS.linux.$version.zip" '@..\..\..\ReleaseIncludes\listfile.linux.txt'
 
 cd "..\..\.."
