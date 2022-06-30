f4f1fb2a48321947b1797dfedec56c06da5a37b7
sfc-gh-rbacastow <ryan.bacastow@snowflake.com>
2022-06-29 21:49:36 -0500

    Working latest binaries import

--- Download-SFBinaries.ps1
+++ Download-SFBinaries.ps1
@@ -1,4 +1,12 @@
-$version_number = Read-Host -Prompt "What version of the binaries do you want to install?"
+$version_number = Read-Host -Prompt "`r`nWhat version of the binaries do you want to install? (Hit enter to get the latest binaries)"
+
+if ($version_number -eq ''){
+    Write-Host "`r`nSearching https://api.github.com/repos/Snowflake-Labs/sfsnowsightextensions/releases/latest for latest binaries...`r`n" -ForegroundColor Cyan
+    $requestData = Invoke-WebRequest -Uri  "https://api.github.com/repos/Snowflake-Labs/sfsnowsightextensions/releases/latest"
+    $releases = ConvertFrom-Json $requestData.content
+    $version_number = $releases.tag_name
+    Write-Host "Found latest release: $version_number`r`n" -ForegroundColor Cyan
+}
 
 Do { $os = Read-Host -Prompt "Which os are you downloading the binaries for? Use osx, win, or linux" }
     while ('osx', 'win', 'linux' -notcontains $os )
@@ -9,4 +17,4 @@ curl "https://github.com/Snowflake-Labs/sfsnowsightextensions/releases/download/
 
 Expand-Archive "~/Downloads/SnowflakePS.$os.$version_number.zip" -DestinationPath "~/Downloads/SnowflakePS.$os.$version_number" -Force
 
-Import-Module "~/Downloads/SnowflakePS.$os.$version_number/SnowflakePS.psd1 -Force"
+Import-Module "~/Downloads/SnowflakePS.$os.$version_number/$os/SnowflakePS.psd1" -Force
