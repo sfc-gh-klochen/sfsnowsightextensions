129cdd32ee020361d53f9244061ed74a86a5851e
Michael Ybarra <michael.ybarra@snowflake.com>
2022-07-15 11:28:15 -0700

    ZipRelease update

--- ZipReleases.ps1
+++ ZipReleases.ps1
@@ -1,5 +1,14 @@
-#Native compression apps may not be executable from a script. 7-Zip works good. Make sure to update the Path accordingly.
-$zip = "/Users/mybarra/Downloads/7z2107-mac/7zz"
+# Get 7Zip path
+if ($PSVersionTable.Platform -eq "Win32NT")
+{
+    # Windows
+    $zip = "C:\Program Files\7-Zip\7z.exe"
+}
+elseif (($PSVersionTable.Platform -eq "Unix") -and ($PSVersionTable.OS.Contains("Darwin") -eq $true))
+{
+    # Mac
+    $zip = "~/Downloads/7z2107-mac/7zz"
+}
 
 # Get version from the project file
 $projXML = [xml](Get-Content -Path .\SnowflakePS.csproj)
@@ -11,6 +20,9 @@ cd "bin/Publish/win"
 
 cd "../osx"
 & $zip a "../../../../Releases/$version/SnowflakePS.osx.$version.zip" '@../../../ReleaseIncludes/listfile.osx.txt'
+
+cd "../osx-arm"
+& $zip a "../../../../Releases/$version/SnowflakePS.osx-arm.$version.zip" '@../../../ReleaseIncludes/listfile.osx.txt'
 
 cd "../linux"
 & $zip a "../../../../Releases/$version/SnowflakePS.linux.$version.zip" '@../../../ReleaseIncludes/listfile.linux.txt'
