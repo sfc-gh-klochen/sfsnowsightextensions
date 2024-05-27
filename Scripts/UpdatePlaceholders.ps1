198c321df02056902ec5cd560f398b4367b77840
Joel Barrantes <joel.barrantes@snowflake.com>
2024-05-26 23:30:49 -0600

    Update UpdatePlaceholders.ps1

--- Scripts/UpdatePlaceholders.ps1
+++ Scripts/UpdatePlaceholders.ps1
@@ -31,13 +31,13 @@ Get-Content -Path $RunSettingsPath
 #Update ps1 placeholders
 $PSScriptContent = Get-Content -Path $PowerShellScriptPath
 
-$PSScriptContent = $PSScriptContent -replace '$extension_path = "{EXTENSION_PATH}"', ('$extension_path = "{0}"' -f $extension_path)
-$PSScriptContent = $PSScriptContent -replace '$account_1 = "{ACCOUNT_1}"', ('$account_1 = "{0}"' -f  $account_1)
-$PSScriptContent = $PSScriptContent -replace '$account_2 = "{ACCOUNT_2}"', ('$account_2 = "{0}"' -f  $account_2)
-$PSScriptContent = $PSScriptContent -replace '$user_name_1 = "{USER_1}"', ('$user_name_1 = "{0}"' -f  $user_name_1)
-$PSScriptContent = $PSScriptContent -replace '$user_name_2 = "{USER_2}"', ('$user_name_2 = "{0}"' -f  $user_name_2)
-$PSScriptContent = $PSScriptContent -replace '$password_1 = "{PASSWORD_1}"', ('$password_1 = "{0}"' -f  $password_1)
-$PSScriptContent = $PSScriptContent -replace '$password_2 = "{PASSWORD_2}"', ('$password_2 = "{0}"' -f  $password_2)
+$PSScriptContent = $PSScriptContent -replace 'extension_path = "{EXTENSION_PATH}"', ('extension_path = "{0}"' -f $extension_path)
+$PSScriptContent = $PSScriptContent -replace 'account_1 = "{ACCOUNT_1}"', ('account_1 = "{0}"' -f  $account_1)
+$PSScriptContent = $PSScriptContent -replace 'account_2 = "{ACCOUNT_2}"', ('account_2 = "{0}"' -f  $account_2)
+$PSScriptContent = $PSScriptContent -replace 'user_name_1 = "{USER_1}"', ('user_name_1 = "{0}"' -f  $user_name_1)
+$PSScriptContent = $PSScriptContent -replace 'user_name_2 = "{USER_2}"', ('user_name_2 = "{0}"' -f  $user_name_2)
+$PSScriptContent = $PSScriptContent -replace 'password_1 = "{PASSWORD_1}"', ('password_1 = "{0}"' -f  $password_1)
+$PSScriptContent = $PSScriptContent -replace 'password_2 = "{PASSWORD_2}"', ('password_2 = "{0}"' -f  $password_2)
 $PSScriptContent | Set-Content -Path $PowerShellScriptPath
 Get-Content -Path $PowerShellScriptPath
 
